fd34255cb2fb4c326974310710b1758138299a34
Bradley Murchison <bradleymurchison@gmail.com>
2018-07-19 21:52:03 -0500

    started to create a checkLetter function

--- js/app.js
+++ js/app.js
@@ -13,7 +13,8 @@ const keyboardDiv = document.querySelector('#qwerty');
 const phraseDiv = document.querySelector('#phrase');
 const startBtn = document.querySelector('.btn__reset');
 const UL = document.querySelector('#phrase ul');
-let randomPhraseCharacters =  getRandomPhraseAsArray(phrases);
+const PhraseCharacters =  getRandomPhraseAsArray(phrases);
+const letters = document.getElementsByClassName('letter');
 
 function getRandomPhraseAsArray(array) {
     function getRandomNum(array) {
@@ -24,19 +25,38 @@ function getRandomPhraseAsArray(array) {
     const characterArray = randomPhrase.split('');
     return characterArray;
 }
-
-function addPhraseToDisplay() {
-    for (let i = 0; i < randomPhraseCharacters.length; i++) {
-        const character = randomPhraseCharacters[i];
+function addPhraseToDisplay(array) {
+    for (let i = 0; i < array.length; i++) {
+        const character = array[i];
         const listItem = document.createElement('li');
         UL.appendChild(listItem);
         listItem.textContent = character;
+
+        if (listItem.textContent !== " " ) {
+            listItem.classList.add('letter');
+        }
     }
 }
+
+function checkLetter(button) {
+    for (let i = 0; i < PhraseCharacters.length; i++) {
+        if (button.textContent === letters[i].textContent) {
+            const matchedLetter = letters[i];
+            matchedLetter.classList.add('show');
+        }
+    }
+}
+
+keyboardDiv.addEventListener('click', (e) => {
+    e.target.classList.add('chosen');
+    e.target.setAttribute("disabled", "disabled");
+
+    const letterFound = checkLetter(e.target);
+});
 
 startBtn.addEventListener('click', () => {
     const overlay = document.querySelector('#overlay');
     overlay.style.display = 'none';
 });
 
-addPhraseToDisplay();
+addPhraseToDisplay(PhraseCharacters);
